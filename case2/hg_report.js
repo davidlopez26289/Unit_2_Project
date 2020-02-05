f4aa5b7a9beae889fa261748fbeed87f7de22432
David Lopez <dlopez222@west-mec.org>
2020-02-04 18:30:32 -0700

    3 step done on JS

---
+++ case2/hg_report.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: 
+   Date:   
+   
+   Filename: hg_report.js
+	
+*/
+for(var i = 0; i < item.length; i++){
+   var gameReport = "<h1>"+ itemTitle[i] +"</h1>"
+   gameReport += "<h2>" + "by" + itemManufacturer[i] + "</h2>";
+   gameReport += "<img src = 'hg_id.png' alt ="
+}
