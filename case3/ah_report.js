b99829c6e6ba1b3491283f7684d335136aac5717
David Lopez <dlopez222@west-mec.org>
2020-02-06 17:23:28 -0700

    finished case 3

--- case3/ah_report.js
+++ case3/ah_report.js
@@ -27,14 +27,26 @@
       information for the donor
       
 */
+//creates total money
+var donationTotal = 0;
 
+donors.forEach(calcSum);
 
+var summaryTable = "<table><tr><th>Donors</th><td>" + donors.length + "</td></tr><tr><th>Total Donations</th><td>$" + donationTotal.toLocaleString() + "</td></tr></table>"
 
+document.getElementById("donationSummary").innerHTML = summaryTable;
 
+var majorDonors = donors.filter(findMajorDonors);
 
+majorDonors.sort(donorSortDescending);
 
+var donorTable = "<table> <caption>Major Donors</caption> <tr><th>Donation</th><th>Donor ID</th> <th>Date</th><th>Name</th><th>Address</th> <th>Phone</th><th>E-mail</th></tr>";
 
+majorDonors.forEach(writeDonorRow);
 
+donorTable += "</table>";
+
+document.getElementById("donorTable").innerHTML = donorTable;
 
 
 function calcSum(donorAmt) {
